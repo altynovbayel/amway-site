f1afd425aa400ba3287e27c87676727a01528854
sattarzhanovdev <sattarzhanovdev@gmail.com>
2023-05-07 13:46:51 +0600

    form

--- src/config/api.js
+++ src/config/api.js
@@ -3,4 +3,17 @@ import axios from "axios";
 export const api = {
   getProducts: () => axios.get('/products/'),
   getProduct: (id) => axios.get(`/products/${id}/`),
+  postComment: (data) => fetch(`https://631afc8ddc236c0b1ee91ae6.mockapi.io/api/v1/comments/`, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  }),
+  getComments: () => fetch('https://631afc8ddc236c0b1ee91ae6.mockapi.io/api/v1/comments/', {
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json'
+    }
+  })
 }
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -3,13 +3,28 @@ import { api } from "../config/api"
 
 export const GetProducts = () => {
   const [ products, setProducts ] = React.useState(null)
-
+  
   React.useEffect(() => {
     api.getProducts()
-      .then(res => setProducts(res.data))
+    .then(res => setProducts(res.data))
+  }, [])
+  
+  return {
+    products
+  }
+}
+
+
+export const GetComments = () => {
+  const [ comments, setComments ] = React.useState(null)
+  
+  React.useEffect(() => {
+    api.getComments()
+      .then(res => res.json())
+      .then(res => setComments(res))
   }, [])
 
   return {
-    products
+    comments
   }
 }
